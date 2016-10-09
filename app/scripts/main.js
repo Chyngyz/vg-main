6551ffcbfdd70d4ba0d6dbf57c9768457f13aecd
Chyngyz Arystan uulu <camphosis@gmail.com>
2016-10-09 22:45:21 +0600

    Rebuild

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,10 @@
   $(document).ready(function () {
     $('body').vegas({
         slides: [
-            { src: '/images/slide-1.jpg' },
-            { src: '/images/slide-2.jpg' },
-            { src: '/images/slide-3.jpg' },
-            { src: '/images/slide-4.jpg' }
+            { src: 'images/slide-1.jpg' },
+            { src: 'images/slide-2.jpg' },
+            { src: 'images/slide-3.jpg' },
+            { src: 'images/slide-4.jpg' }
         ],
         overlay: true,
         animation: ['kenburns']
